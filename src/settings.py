63204ad736326c317373579b72e3b90bd7d10f96
Ars Vladimirov <grokhael@gmail.com>
2023-08-18 11:37:51 +0600

    upd

--- src/settings.py
+++ src/settings.py
@@ -15,6 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Chest Xray Masks and Labels"
 PROJECT_NAME_FULL: str = "Chest Xray Masks and Labels"
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
@@ -83,6 +84,7 @@ def get_settings():
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
+        "hide_dataset": HIDE_DATASET,
         "applications": APPLICATIONS,
         "category": CATEGORY,
         "cv_tasks": CV_TASKS,
